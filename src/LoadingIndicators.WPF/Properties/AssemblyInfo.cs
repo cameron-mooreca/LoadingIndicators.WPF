c17886de1bdff4fa6d61f19657956fa3bd82c929
Cameron Moore-Carter <cameron.moore-carter@fphcare.co.nz>
2025-06-09 17:26:04 +1200

    Pull all package bs out and upgrade to NET 8

--- src/LoadingIndicators.WPF/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Markup;
-
-[assembly: ComVisible(false)]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-[assembly:XmlnsPrefix("http://github.com/zeluisping/loadingIndicators/xaml/controls", "li")]
-[assembly:XmlnsDefinition("http://github.com/zeluisping/loadingIndicators/xaml/controls", "LoadingIndicators.WPF")]
